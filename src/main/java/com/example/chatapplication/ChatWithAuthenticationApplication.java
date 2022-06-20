43e353efd212f28e796ea595768a70bf8b79f35f
probro27 <prabhavkhera@hotmail.com>
2022-06-20 01:30:45 -0400

    first commit

---
+++ src/main/java/com/example/chatapplication/ChatWithAuthenticationApplication.java
@@ -0,0 +1,14 @@
+package com.example.chatapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class ChatWithAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatWithAuthenticationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/chatapplication/ChatWithAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chatapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatWithAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
